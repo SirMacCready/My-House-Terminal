f7a6cb97833090a2311ddee46078e725a6f4572c
SirMacCready <polocharbel@gmail.com>
2023-10-28 18:22:06 +0200

    TypeWrite Debug

--- src/js/TypeWrite.js
+++ src/js/TypeWrite.js
@@ -1,46 +1,35 @@
 const idNames = [];
+
 // Use jQuery to select all elements with an id attribute
-$('[id]').each(function() {
-    const idAttr = $(this).attr('id');
-
-    // Split the id attribute by spaces to get individual id names
-    const ids = idAttr.split(' ');
-
-    // Add the individual id names to the idNames array
-    idNames.push(...ids);
+$('[id]').each(function () {
+    idNames.push(...$(this).attr('id').split(' '));
 });
 
-let initspeed = 50;
+const initspeed = 50;
 
 function textloading(id, speed) {
-    try {
-    return new Promise((resolve) => {
-        $(`#${id}`).typewrite({
-            'delay': speed, // Time in ms between each letter
-            'extra_char': '|', // "cursor" character to append after each display
-            'trim': true, // Trim the string to type (Default: false, does not trim)
-            'callback': function () {
-                resolve(); // Resolve the promise when text loading is complete
-            }
-        });
-    });
-} 
-catch {
-    console.log("error :" + error );
-}  }
-
-function loadWebsite() {
-    
-    try{
-    let promiseChain = Promise.resolve();
-    idNames.forEach(id => {
-        textloading(id, initspeed);
+    return new Promise((resolve, reject) => {
+        try {
+            $(`#${id}`).typewrite({
+                'delay': speed,
+                'extra_char': '|',
+                'trim': true,
+                'callback': resolve
+            });
+        } catch (error) {
+            console.log("Error: " + error);
+            reject(error);
+        }
     });
 }
-catch {
-    console.log("error :" + error)
-}}
 
+async function loadWebsite() {
+    try {
+        await Promise.all(idNames.map(id => textloading(id, initspeed)));
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
 
 loadWebsite();
 
